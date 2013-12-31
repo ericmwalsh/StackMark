86e88cb25e067804ac186bd72c2e31c9d0a361ad
Jauhar Basrai <jbasrai@uci.edu>
2013-12-31 10:02:38 -0800

    commited wrong assets last time, this is the right one

--- popup.js
+++ popup.js
@@ -1,27 +1,120 @@
+//MAIN
+var bkg = chrome.extension.getBackgroundPage();
+
 $(function() {
-	chrome.storage.sync.get(null, function(items) {
-		console.log(items);
-		var $ul = $('#sites');
+	makePopup();
 
-		for(date in items) {
-			var item = items[date];
-			var title = (function() {
-				if(item.title.length > 43) {
-					return item.title.slice(0,40) + '...';
-				}
-				else {
-					return item.title;
-				}
-			})();
-
-			var $li = $('<li>');
-
-			var $a = $('<a href="' + item.url + '">' + title + '</a></li>');
-			var $img = $('<img src="' + item.furl + '" height="16" width="16">');
-
-			$a.prepend($img);
-			$li.append($a);
-			$ul.append($li);
-		}
+	$('#action').click(function() {
+		modifyStack(bkg.stacked);
 	});
 });
+
+//fetches stack from storage and renders list in DOM
+function makePopup() {
+	chrome.storage.sync.get(null, function(items) {
+		renderButton();
+		renderList(items);
+	});
+};
+
+//chooses appropriate color and text of the button, 
+//according to saved state of the page
+function renderButton() {
+	if(bkg.stacked !== null) {
+		$('#action').text('Remove this page');
+	}
+	else {
+		$('#action').text('Add this page');	
+	}
+};
+
+//toggles button between the add and remove state
+function toggleButton() {
+	if($('#action').text() == 'Add this page') {
+		$('#action').text('Remove this page');
+	}
+	else {
+		$('#action').text('Add this page');	
+	}
+}
+
+//HELPER: generateList()
+//renders sites list in DOM
+function renderList(items) {
+	
+	for(date in items) {
+		var item = items[date];
+		appendSiteToList(item, date);
+	}
+};
+
+//self explanatory
+//item: {url, title, furl}
+function appendSiteToList(item, date) {
+	var $ul = $('#sites');
+
+	var shortTitle = item.title.length > 43 ? item.title.slice(0,40) + '...' : item.title;
+
+	var $li = $('<li>').click((function(url, date) {
+		return function() {
+			chrome.storage.sync.remove(date);
+			window.open(url, '_blank');	
+		}
+	})(item.url, date.toString()));
+
+	var $a = $('<a title="' + item.title + '" target="_blank">' + shortTitle + '</a></li>');
+	var $img = $('<img src="' + item.furl + '" height="16" width="16">');
+
+	$a.prepend($img);
+	$li.append($a);
+	$ul.append($li);
+}
+
+//callback for when the button is pressed
+function modifyStack(stacked) {
+	var url, title, furl, d, obj={}, info={};
+	d = Date.now();
+	var queryHelper = {'active': true, 'currentWindow': true};
+	var defTitle = 'StackMark\n\nLeft click to add to your list.\nRight click to bookmark and list.';
+	var newTitle = 'StackMark\n\nLeft click to remove from list.'
+
+	chrome.tabs.query(queryHelper, function (tabs) {
+    	url = tabs[0].url;
+    	title = tabs[0].title;
+    	furl = tabs[0].favIconUrl;
+    	// If the page hasn't finished loading then nothing will happen.
+    	if(!url || !title){
+			console.log('ERROR NO URL OR TITLE!');
+			return;
+		}
+		else{
+			// Remove an already 'stacked' item from the list.
+			if(stacked !== null){
+				toggleButton();
+
+				chrome.storage.sync.remove(stacked.toString(), function(){
+					chrome.browserAction.setIcon({path:'icon.png'}, function(){
+						chrome.browserAction.setTitle({title:defTitle});
+						stacked = null;
+					});
+				});
+			}
+			// Add a new item to the stack.
+			else{
+				info = {url:url, title:title, furl:furl };
+				
+				appendSiteToList(info, d);
+				toggleButton();
+
+				obj[d]=info;
+				chrome.storage.sync.set(obj, function(){
+						chrome.browserAction.setIcon({path:'icon2.png'}, function(){
+							chrome.browserAction.setTitle({title:newTitle});
+							stacked = d;
+						});
+					
+				});
+			}
+		}
+	});
+};
--- saveToList.js
+++ saveToList.js
@@ -1,7 +1,10 @@
 // Main script file that listens for the button click and then adds it to the list.
 // Also makes sure the icon/title appropriately reflect if a website is 'Stacked' or not.
+
 var stacked = null;
+
 chrome.tabs.onActivated.addListener(function(){
+
 	chrome.storage.sync.get(null, function(items) {
 		var queryHelper = {'active': true, 'currentWindow': true};
 		var defTitle = 'StackMark\n\nLeft click to add to your list.\nRight click to bookmark and list.';
@@ -12,13 +15,7 @@ chrome.tabs.onActivated.addListener(function(){
 	    var allKeys = Object.keys(items);
 	    chrome.tabs.query(queryHelper, function(tabs){
 	    	x = tabs[0].url;
-	    	var y = tabs[0].favIconUrl, z = tabs[0].title;
-	    	//CHECK IF IT'S THE NEWTABPAGE
-			if(z == 'New Tab'){
-				if(y == 'https://www.google.com/favicon.ico' || x == 'chrome-search://local-ntp/local-ntp.html'){
-					console.log('changes to new tab page');
-				}
-			}
+	    	
 	    	allKeys.forEach(function(key){
 				if(items[key].url == x){
 					checkStorage=true;
@@ -49,51 +46,7 @@ chrome.tabs.onActivated.addListener(function(){
                         }
                 });
 			}
+			
 	    });
 	});
 });
-
-//Called when the user clicks on the StackMark icon
-function markIt() {
-	var url, title, furl, d, obj={}, info={};
-	d = Date.now();
-	var queryHelper = {'active': true, 'currentWindow': true};
-	var defTitle = 'StackMark\n\nLeft click to add to your list.\nRight click to bookmark and list.';
-	var newTitle = 'StackMark\n\nLeft click to remove from list.'
-
-	chrome.tabs.query(queryHelper, function (tabs) {
-    	url = tabs[0].url;
-    	title = tabs[0].title;
-    	furl = tabs[0].favIconUrl;
-    	// If the page hasn't finished loading then nothing will happen.
-    	if(!url || !title){
-			console.log('ERROR NO URL OR TITLE!');
-			return;
-		}
-		else{
-			// Remove an already 'stacked' item from the list.
-			if(stacked){
-				//var tempD= d.toString();
-				chrome.storage.sync.remove(stacked.toString(), function(){
-					chrome.browserAction.setIcon({path:'icon.png'}, function(){
-						chrome.browserAction.setTitle({title:defTitle});
-						stacked = null;
-					});
-				});
-			}
-			// Add a new item to the stack.
-			else{
-				info = {'url':url, 'title':title, 'furl':furl };
-				obj[d]=info;
-				chrome.storage.sync.set(obj, function(){
-						chrome.browserAction.setIcon({path:'icon2.png'}, function(){
-							chrome.browserAction.setTitle({title:newTitle});
-							stacked = d;
-						});
-					
-				});
-			}
-		}
-	});
-	
-};
